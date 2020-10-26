630d93ca0359f1cbfec79ed9bce1f9f004d14c83
Helvecios <helvecios@globo.com>
2020-10-26 12:40:17 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -2,12 +2,21 @@ package com.example.course.entities;
 
 import java.io.Serializable;
 
-//usar o Serializable para que os objetos possam trafegar na rede
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+//usar o Serializable para que os objetos possam trafegar na rede
+//annotation
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	//atributos
+	//annotation para indicar qual será a chave primaria da tabela do BD (id)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
