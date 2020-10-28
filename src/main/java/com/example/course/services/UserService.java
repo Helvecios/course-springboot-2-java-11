ff12d1456adade850d3bce599c60c06ac24c355b
Helvecios <helvecios@globo.com>
2020-10-28 18:31:58 -0300

    Exception handling - delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 @Service //annotation
@@ -34,7 +37,14 @@ public class UserService {
 	
 	//método para deletar um usuário no BD
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+				
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	//método para atualizar usuário no BD
